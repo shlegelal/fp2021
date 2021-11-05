8e18dbf22fef8c939092c39c96ded6be16c9caca
shlegelal <shlegel.alink0@gmail.com>
2021-11-05 23:55:22 +0300

    Add ast

--- asm/lib/ast.mli
+++ asm/lib/ast.mli
@@ -1,11 +1,22 @@
-type name = string
+(** The main type for asm AST *)
+type script = 
+| start * section_data * section_text             (* global _start   section .data   section .text *)
+| start * section_text                            (* global _start   section .text *)
 
-(** The main type for our AST (дерева абстрактного синтаксиса) *)
-type 'name t =
-  | Var of 'name (** Variable [x] *)
-  | Abs of 'name * 'name t (** Abstraction [λx.t] *)
-  | App of 'name t * 'name t
+and start = string                                (* public _start *)
 
-(* Application [f g ] *)
-(** In type definition above the 3rd constructor is intentionally without documentation
-to test linter *)
+and section_data = var list                       (* section .data *)
+
+and var = string * string * string list
+
+and section_text = text_elem list                 (* section .text *)
+
+and text_elem = 
+| var
+| func
+
+and func = string * command_line list
+
+and command_line = command * string list
+
+and command = string
