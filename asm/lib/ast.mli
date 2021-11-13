0b66d77986ce3a9c79e22c6988072427a8215e1f
shlegelal <shlegel.alink0@gmail.com>
2021-11-13 17:30:55 +0300

    Delete Lambda code from asm

--- asm/lib/ast.mli
+++ asm/lib/ast.mli
@@ -1,9 +1,9 @@
 (** The main type for asm AST *)
 type script = 
-| start * section_data * section_text             (* global _start   section .data   section .text *)
-| start * section_text                            (* global _start   section .text *)
+| WithData of start_code * section_data * section_text             (* global _start   section .data   section .text *)
+| WithoutData of start_code * section_text                            (* global _start   section .text *)
 
-and start = string                                (* public _start *)
+and start_code = string                                (* public _start *)
 
 and section_data = var list                       (* section .data *)
 
@@ -12,8 +12,8 @@ and var = string * string * string list
 and section_text = text_elem list                 (* section .text *)
 
 and text_elem = 
-| var
-| func
+| Var of var
+| Func of func
 
 and func = string * command_line list
 
