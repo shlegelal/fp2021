e1bda1e955f532cb24a3479b81bfb17b9f33b6cd
shlegelal <shlegel.alink0@gmail.com>
2022-01-13 17:08:43 +0300

    fix demo3

--- asm/demos/asm_try/demo3.asm
+++ asm/demos/asm_try/demo3.asm
@@ -3,15 +3,12 @@
 
 ; скалярное произведение
 ;------------------------------
-section .data
- A db 1,2,3,4,5,6 ; вектор 1
-B db 6,5,4,3,2,1  ; вектор 2
 section .text
 
 _start:
 
-mov rbx, A ; A
-   mov rcx, B ; B
+   mov rbx, 0x010203040506 ; вектор с элементами 1 2 3 4 5 6
+   mov rcx, 0x060504030201 ; вектор с элементами 6 5 4 3 2 1
    mov rdx, 0xff
    mov rax, 0xff 
    
@@ -28,12 +25,31 @@ jne @b
 ;------------------------------
 
 ; print number to ascii 
-; 56 -> '8'
+; 56 -> 0x38
+
+section .data
+codes db '0123456789ABCDEF'
+
+section .text
+mov rax, rbp
+mov rdi, 1
+mov rdx, 1
+mov rcx, 64
+.loop push rax
+sub rcx, 4
+sar rax, cl
+and rax, 0xf
+
+lea rsi, [codes + rax]
 mov rax, 1
-  mov rdi, 1
-  mov rsi, rbp ; результат в регистре rbp = 56
-  mov rdx, 1
-  syscall
+push rcx
+syscall
+pop rcx
+pop rax
+
+test rcx, rcx
+jnz .loop
+
 
 ; exit
 mov     rAx, 60
